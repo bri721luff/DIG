c29b5d9ba09d8037f1355784e91ebb85d1edb6fb
bri721luff <78577146+bri721luff@users.noreply.github.com>
2021-02-09 20:05:22 -0700

    Add files via upload

---
+++ Flask/flask.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# @ signifies a decorator
+
+@app.route('/')
+def index():
+  return render_template('form.html')
+
+@app.route('/dashboard/<username>', methods=['GET', 'POST'])
+def dashboard(username):
+	if request.method == 'POST':
+		return "You are using POST"
+	else:
+		return "You are probably using GET"
+
+@app.route('/confirm')
+def confirm():
+	return render_template('confirm.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
