68a336583cea6092606200f7d444191c295d6b22
bri721luff <78577146+bri721luff@users.noreply.github.com>
2021-02-09 21:01:01 -0700

    Update flask.py

--- Flask/flask.py
+++ Flask/flask.py
@@ -7,16 +7,5 @@ app = Flask(__name__)
 def index():
   return render_template('form.html')
 
-@app.route('/dashboard/<username>', methods=['GET', 'POST'])
-def dashboard(username):
-	if request.method == 'POST':
-		return "You are using POST"
-	else:
-		return "You are probably using GET"
-
-@app.route('/confirm')
-def confirm():
-	return render_template('confirm.html')
-
 if __name__ == '__main__':
 	app.run(debug=True)
